75587fba08f539783d2e94ae5613d57ae5a83076
beer-alchimist <profel.game@yandex.ru>
2024-10-27 15:42:33 +0300

    debug file component js slider

--- src/js/component.js
+++ src/js/component.js
@@ -1,5 +1,5 @@
+var count = 0;
 class component{
-    count = 0;
     static display(id){
         var main_el = document.getElementById(id).parentNode.childNodes;
         main_el.forEach(row=>{
@@ -14,32 +14,11 @@ class component{
         })
     }
     static show(num){
+        num+=count;
         const slides = document.querySelectorAll('.slide');
-        var a = num >= slides.length?0:num < 0?slides.length-1:num;
+        count=num>= slides.length?0:num<0?slides.length-1:num;
         slides.forEach(slide=>{
-            slide.style.transform = `translateX(-${a * 100}%)`;
+            slide.style.transform = `translateX(-${count * 100}%)`;
         });
     }
-}
-let currentSlide = 0;
-function showSlide(index) {
-    const slides = document.querySelectorAll('.slide');
-    if (index >= slides.length) {
-        currentSlide = 0;
-    } else if (index < 0) {
-        currentSlide = slides.length - 1;
-    } else {
-        currentSlide = index;
-    }
-
-    slides.forEach((slide, i) => {
-        slide.style.transform = `translateX(-${currentSlide * 100}%)`;
-    });
-}
-
-function changeSlide(direction) {
-    showSlide(currentSlide + direction);
-}
-
-// Показать первый слайд при загрузке
-showSlide(currentSlide);
+}  
